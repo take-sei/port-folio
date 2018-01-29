ad04e8c9d3425327ebcad9cf1fcf696ca1dcbb86
Sei Takeda <takesei@honyamorakeMBP.local>
2018-01-29 13:49:14 +0900

    Created a Scaffold of application and integrated plugins below. Element UI, SASS/Pug loaders.

---
+++ pages/element.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div
+    div.block
+    h4 Element with Nuxt.js!
+    el-rate(v-model="value", :colors="['#2F3D4D', '#64B486', '#4B7C6E']")
+</template>
+
+
+<style scoped lang="sass">
+.block
+  padding: 0 20px
+</style>
+
+<script>
+export default {
+  data() {
+    return {value: null}
+  }
+}
+</script>
