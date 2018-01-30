d5039de208d02d63c438378930440289e2535e7a
take.sei <sei06k14@gmail.com>
2018-01-30 14:13:03 +0900

    :sparkles: title of Headcomp would change when button is pushed.

--- pages/modules/vuex_headcomp.vue
+++ pages/modules/vuex_headcomp.vue
@@ -3,11 +3,12 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex"
 export default {
   name: "headComp",
-  data() {
-    return {title: '感想を入力'}
-  }
+  computed: mapGetters({
+    "title": "getTitle"
+      })
 }
 </script>
 
--- store/index.js
+++ store/index.js
@@ -7,13 +7,37 @@ const Form = {
   actions: {
     buttonAction({commit, state, rootState}) {
       console.log("buttonAction")
+      commit("setStepCount", null, {root: true})
+    }
+  }
+}
+
+const Head = {
+  state:{
+    title: ["感想を入力", "確認画面", "送信完了"]
+  },
+  mutations: {},
+  actions: {},
+  getters: {
+    getTitle (state, getters, rootState){
+      return state.title[rootState.stepCount]
     }
   }
 }
 
 const store = () => new Vuex.Store({
+  state:{
+    stepCount: 0
+  },
+  mutations:{
+    setStepCount(state){
+      console.log("rootsetStepCount")
+      state.stepCount++
+    }
+  },
   modules:{
-    Form
+    Form,
+    Head
   }
 })
 
