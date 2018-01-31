d6ed1938e88250e968c521b4e31601cc958bec59
take.sei <sei06k14@gmail.com>
2018-01-31 21:40:49 +0900

    :tada: Create a form feature using Vuex.js

---
+++ pages/modules/vuex_thanks.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+div
+  p Thanksページ
+  HeadComp
+  p 送信ありがとうございました.
+</template>
+
+<script>
+import HeadComp from "@/pages/modules/vuex_headcomp"
+import { mapActions, mapGetters } from "vuex"
+
+export default{
+  name: "thanks",
+  components: {
+    HeadComp
+  }
+}
+
+</script>
+
+<style lang="sass">
+</style>
